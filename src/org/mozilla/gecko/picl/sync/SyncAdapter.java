9367a0ab173bd7501f6767e1d2613a15c69d26c7
Sean McArthur <sean.monstar@gmail.com>
2013-02-20 16:07:25 -0800

    hello world

---
+++ src/org/mozilla/gecko/picl/sync/SyncAdapter.java
@@ -0,0 +1,22 @@
+package org.mozilla.gecko.picl.sync;
+
+import android.accounts.Account;
+import android.content.AbstractThreadedSyncAdapter;
+import android.content.ContentProviderClient;
+import android.content.Context;
+import android.content.SyncResult;
+import android.os.Bundle;
+
+public class SyncAdapter extends AbstractThreadedSyncAdapter {
+
+	public SyncAdapter(Context context, boolean autoInitialize) {
+		super(context, autoInitialize);
+	}
+
+	@Override
+	public void onPerformSync(Account account, Bundle extras, String authority,
+			ContentProviderClient provider, SyncResult syncResult) {
+		
+	}
+
+}
